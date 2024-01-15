6aeda50ea62355d7300126be06d23d87036485e9
Peter Hayward <peterhayward1707@gmail.com>
2024-01-15 15:41:47 +0000

    Completed menu system and function heads

---
+++ Guava.py
@@ -0,0 +1,39 @@
+def main():
+    print ("Please select an activity from the following options:")
+    print ("[1] Sorting Integers\n[2] Noughts and Crosses")
+    menu = input ("[1/2]:")
+
+    if menu == "1":
+        print ("\n[SORTING INTEGERS]\n")
+        intsort()
+
+    if menu == "2":
+        print ("\n[NOUGHTS AND CROSSES]\n")
+        noughts()
+
+    else:
+        print ("\nplease enter a listed number.\n")
+        main()
+
+def intsort():
+   
+
+    print ("un-sorted list:")
+    print (list)
+
+
+
+
+    
+    main()
+
+def noughts():
+    
+    
+    
+    main()
+
+main()
+   
+
+
