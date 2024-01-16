0ab797a239141f6df9901faa78cd31abff63663a
Peter Hayward <peterhayward1707@gmail.com>
2024-01-16 19:34:23 +0000

    created bubble sort for list values

--- Guava.py
+++ Guava.py
@@ -1,21 +1,21 @@
 def main():
-    print ("Please select an activity from the following options:")
+    print ("Please select an activity from the following options:") #menu system
     print ("[1] Sorting Integers\n[2] Noughts and Crosses")
     menu = input ("[1/2]:")
 
     if menu == "1":
         print ("\n[SORTING INTEGERS]\n")
-        intsort()
+        int_sort()
 
     if menu == "2":
         print ("\n[NOUGHTS AND CROSSES]\n")
-        noughts()
+        noughts_crosses()
 
     else:
         print ("\nplease enter a listed number.\n")
         main()
 
-def intsort():
+def int_sort(): #problem 2 integer sort
     
     list = []
     listDone = False
@@ -23,14 +23,14 @@ def intsort():
     print ("Enter the integers you wish to be sorted,")
     print ("Once all numbers have been entered, enter 'exit' to continue.")
 
-    while listDone != True:
+    while listDone != True: #loop for user to enter unlimited numbers
         userNum = input ("Enter a number:")
 
         if userNum == "exit":
             listDone = True
 
         try:
-            userNum = float(userNum)
+            userNum = float(userNum) #validation for user entered numbers
         except:
             print ("Please enter a numeric value,")
             continue
@@ -38,18 +38,30 @@ def intsort():
         userNum = float(userNum)
 
         list.append(userNum)
-        print (str(userNum) + " added to list")
+        print (str(userNum) + " added to list") #show user the number they entered
 
-    print ("\nun-sorted list:")
-    print (list)
+    bubble_sort(list) #calls sorting function
 
-
-
+    print ("\nList of values in ascending order:") #prints list values in ascending order
+    print(list)
 
     
+
     main()
 
-def noughts():
+def bubble_sort(list):
+
+    length = len(list)
+
+    for x in range(length-1): #iterate through the list
+        for y in range(0, length-x-1):
+  
+            if list[y] > list[y+1]: #check if list is already swapped  
+                list[y], list[y+1] = list[y+1], list[y] #swap elements
+    
+    return(list) #return to wider process
+
+def noughts_crosses():
     
     
     
