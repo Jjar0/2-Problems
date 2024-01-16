f3bf8a45a6c0758a3e3df9dfe2de5c9962741d69
Peter Hayward <peterhayward1707@gmail.com>
2024-01-16 22:57:18 +0000

    Create functional position dict.

--- noughts_crosses.py
+++ noughts_crosses.py
@@ -1,5 +1,18 @@
 def start():
 
-
+    test_board()
 
     return
+
+def test_board():
+
+    pos = {1:" ",2:" ",3:" ",4:" ",5:" ",6:" ",7:" ",8:" ",9:" "}
+    
+    print (f" {pos[1]} | {pos[2]} | {pos[3]} ")
+    print (f"---+---+---")
+    print (f" {pos[4]} | {pos[5]} | {pos[6]} ")
+    print ("---+---+---")
+    print (f" {pos[7]} | {pos[8]} | {pos[9]} ")
+    
+
+start()
