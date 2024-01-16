98cdf485a69767836ce4691cbee100acf396d71c
Peter Hayward <peterhayward1707@gmail.com>
2024-01-16 21:43:57 +0000

    created tic tac toe file

--- app_menu.py
+++ app_menu.py
@@ -1,5 +1,3 @@
-from integer_sorting import int_sort
-
 while True:
     print ("\nPlease select an activity from the following options:") #menu system
     print ("[1] Sorting Integers\n[2] Noughts and Crosses")
@@ -7,11 +5,14 @@ while True:
 
     if menu == "1":
         print ("\n[SORTING INTEGERS]")
+        from integer_sorting import int_sort
         int_sort()
 
     if menu == "2":
         print ("\n[NOUGHTS AND CROSSES]\n")
-        
+        from noughts_crosses import start
+        start()
+
     else:
         print ("\nplease enter a listed number.\n")
         continue
